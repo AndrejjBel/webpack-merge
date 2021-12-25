ca28d60fd984424bea6f5f0e5e646773eb055ef2
Andrey <84500846+AndrejjBel@users.noreply.github.com>
2021-12-25 22:06:20 +0300

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,1 +1,49 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
+module.exports = {
+  mode: 'development',
+  entry: [
+        './src/app.js'
+    ],
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ],
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+    //minimize: true,
+  },
+  plugins: [new MiniCssExtractPlugin({filename: 'main.min.css'})]
+};
